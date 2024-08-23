eba353af4b85edd8984632671a60c04dfddfed9e
MOHEBBULLAHDEV <mohebbullah.dev@gmail.com>
2024-08-23 12:21:57 +0600

    complete design section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       colors: {
         clifford: '#da373d',
         "btn-color": '#ABEF5F',
+        "ground-color":'background: linear-gradient(0.00deg, rgb(255, 255, 255),rgba(255, 255, 255, 0) 100%);'
       }
     }
   },
